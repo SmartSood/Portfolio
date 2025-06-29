69fd717ecd779e1db6620faaa1f6060666328f4c
Ubuntu <ubuntu@ip-172-31-9-184.ap-south-1.compute.internal>
2025-06-29 02:45:49 +0000

    dkjcb

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
   server: {
     port: 5173,
     host: true,
-    open: true
+    open: false,
+   allowedHosts: ['smarthsood.com', 'www.smarthsood.com'], 
   },
   build: {
     outDir: 'dist',
